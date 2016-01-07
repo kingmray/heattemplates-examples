9148a72c010615858a9675cf2d079fce92b6c099
kingmray <raymond.schmidt@googlemail.com>
2016-01-07 14:52:36 +0000

    initial saltmaster setup for spryker

---
+++ example-project/scripts/install_saltmaster.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# wait for a valid network configuration
+until ping -c 1 syseleven.de; do sleep 5; done
+
+export DEBIAN_FRONTEND=noninteractive
+apt-get update
+apt-get install -y avahi-daemon avahi-utils haveged git curl screen bc wget
+
+# nginx version:
+# install salt master
+apt-get install -y salt-master
+
+mkdir /srv/salt/states
+mkdir /srv/salt/pillar
+
+git clone https://github.com/spryker/saltstack.git /srv/salt/states
+git clone https://github.com/spryker/pillar.git /srv/salt/pillar
+
+cat <<EOF> /etc/salt/master
+interface: 0.0.0.0
+file_roots:
+  base:
+    - /srv/salt/states
+pillar_roots:
+  base:
+    - /srv/salt/pillar
+EOF
+
+service salt-master restart
+
+## echo "* * * * * root /usr/local/sbin/update_sessionconfig >> /var/log/sessionconfig.log" > /etc/cron.d/update_sessionstore
+
+logger "finished salt master installation"
---
+++ example-project/scripts/install_spryker.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# wait for a valid network configuration
+until ping -c 1 syseleven.de; do sleep 5; done
+
+export DEBIAN_FRONTEND=noninteractive
+apt-get update
+apt-get install -y avahi-daemon avahi-utils haveged git curl screen bc wget salt-minion
+
+cat <<EOF> /etc/salt/minion
+master: saltmaster.local
+EOF
+
+#echo "* * * * * root /usr/local/sbin/update_sessionconfig >> /var/log/sessionconfig.log" > /etc/cron.d/update_sessionstore
+
+logger "finished appserver installation"
